c979e99d3ac95f8b2ecf42c2971e2389420cb45d
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2022-12-26 18:11:05 -0600

    Modificacion mongo.js

--- Mission-Backend/Modulo 5 - Bases de datos y NodeJs/TODO/routes/mongo.js
+++
@@ -1,6 +0,0 @@
-//DEPRECATE
-
-// var mongoClient = require("mongodb").MongoClient;
-// mongoClient.connect("mongodb://miservermongotest:em9a0rtVV1KrScBsc7vnP5knVrFlGlUpiDfLsKZ6QgXbEDArROLpTF0m9SfKXWnewJxyOaEGXLl7ACDbiGcmtQ%3D%3D@miservermongotest.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@miservermongotest@", function (err, client) {
-//   client.close();
-// });
